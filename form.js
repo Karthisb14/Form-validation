fb5fa3768ba8bbc11a232a6627408836b334fb2e
Karthikeyan SB <Karthi14061999@gmail.com>
2023-03-22 14:00:24 +0530

    delete table row

--- form.js
+++ form.js
@@ -1,6 +1,8 @@
 //form element
 const form = document.getElementById("myform");
-console.log(firstname);
+const formsubmit = document.getElementById("mytable");
+
+// console.log(firstname);
 
 form.addEventListener("submit", e => {
 
@@ -11,12 +13,63 @@ form.addEventListener("submit", e => {
     let pwd = pwdValidate();
     let phone = phoneValidate();
 
+    form.classList.add('show')
 
+    var arrayitem = JSON.parse(localStorage.getItem("arrayitem") || "[]");
 
-    if(fname && lname && email && pwd && phone){
-        alert(`firstname:${fname}\nlastname:${lname}\nemail:${email}\npassword:${pwd}\nphonenumber:${phone}\n`)
+    let obj = {
+        firstName: fname,
+        lastName: lname,
+        Email: email,
+        Password: pwd,
+        Phone: phone
     }
+
+    arrayitem.push(obj);
+
+    if (Object.keys(obj).every((x) => obj[x] !== undefined)) {
+        localStorage.setItem("arrayitem", JSON.stringify(arrayitem));
+    }
+
+    let txt = "";
+
+    arrayitem.map(element => {
+
+        if(Object.keys(element).every((x) => element[x] !== undefined)){
+            txt += `<tr>
+            <td>${element.firstName}</td>
+            <td>${element.lastName}</td>
+            <td>${element.Email}</td>
+            <td>${element.Password}</td>
+            <td>${element.Phone}</td>   
+            <td><button onclick="deleteItem('${element.Email}')" class="btn btn-success" >Delete</button></td>
+            </tr>`
+        }    
+    });
+
+    formsubmit.innerHTML = txt;
+
+    document.getElementById("hidTable").style.display = "block";
+
+    // if(fname && lname && email && pwd && phone){
+    //     formsubmit.innerHTML = 'success';
+    //     // alert(`firstname:${fname}\nlastname:${lname}\nemail:${email}\npassword:${pwd}\nphonenumber:${phone}\n`)
+    // }
 })
+
+function deleteItem(item) {
+
+    for (var i = 0; i < formsubmit.rows.length; i++) {
+        if (formsubmit.rows[i].cells[2].innerText === item) {
+            formsubmit.deleteRow(i);
+        }
+    }
+    var allitem = JSON.parse(localStorage.getItem("arrayitem"));
+    const newitem = allitem.filter(x => x.Email !== item);
+
+    localStorage.setItem("arrayitem", JSON.stringify(newitem));
+
+};
 
 //input element
 var firstname = document.getElementById("firstname");
@@ -32,38 +85,37 @@ const emailerror = document.getElementById("emailerror");
 const pwderror = document.getElementById("pwderror");
 const phoneerror = document.getElementById("phoneerror");
 
-firstname.addEventListener("keypress", ()=>{
+firstname.addEventListener("keypress", () => {
 
     fnameValidate();
-    
+
 })
 
 
-lastname.addEventListener("keypress", ()=>{
+lastname.addEventListener("keypress", () => {
 
     lnameValidate();
 })
 
-email.addEventListener("keypress", ()=>{
+email.addEventListener("keypress", () => {
 
     emailValidate();
 
 })
 
-phonenumber.addEventListener("keypress", ()=>{
-    phoneValidate();  
+phonenumber.addEventListener("keypress", () => {
+    phoneValidate();
 })
 
-password.addEventListener("keypress", ()=>{
+password.addEventListener("keypress", () => {
     pwdValidate();
 })
 
 
 
-function fnameValidate(){
+function fnameValidate() {
     if (firstname.value === '') {
         fnameerror.innerHTML = 'FirstName is Required!';
-        return false;
     } else if (firstname.value) {
         fnameerror.innerHTML = '';
         return firstname.value;
@@ -71,55 +123,51 @@ function fnameValidate(){
 
 }
 
-function lnameValidate(){
+function lnameValidate() {
     if (lastname.value === '') {
         lastnameerror.innerHTML = 'Lastname is Required!';
-        return false;
     } else if (lastname.value) {
         lastnameerror.innerHTML = '';
         return lastname.value;
     }
 
 }
-function emailValidate(){
+function emailValidate() {
 
     var emailvalid = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (email.value === '') {
         emailerror.innerHTML = 'Email is Required!';
-        return false;
     } else if (!email.value.match(emailvalid)) {
         emailerror.innerHTML = 'Please enter valid Email';
         return false;
-    }else{
+    } else {
         emailerror.innerHTML = '';
         return email.value;
     }
 
 }
 
-function pwdValidate(){
+function pwdValidate() {
     if (password.value === '') {
         pwderror.innerHTML = 'Please Enter the Password!';
-        return false;
     } else if (password.value.length < 6) {
         pwderror.innerHTML = 'Please enter Strong Password!';
         return false;
-    }else{
+    } else {
         pwderror.innerHTML = '';
         return password.value;
     }
 
 }
 
-function phoneValidate(){
+function phoneValidate() {
     var phoneno = /^\d{10}$/;
     if (phonenumber.value === '') {
         phoneerror.innerHTML = 'phonenumber is Required!';
-        return false;
-    } else if (phonenumber.value.match(phoneno)) { 
-       phoneerror.innerHTML = '';
-       return phonenumber.value;
-    }else{
+    } else if (phonenumber.value.match(phoneno)) {
+        phoneerror.innerHTML = '';
+        return phonenumber.value;
+    } else {
         phoneerror.innerHTML = 'Please enter Correct Mobile Number!'
         return false;
     }
